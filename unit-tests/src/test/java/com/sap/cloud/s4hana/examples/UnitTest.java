62c69e50c08437606171b07ee7ee717fcdc43c88
Sander Wozniak <sander.wozniak@sap.com>
2018-06-26 17:24:19 +0200

    Initial commit

---
+++ unit-tests/src/test/java/com/sap/cloud/s4hana/examples/UnitTest.java
@@ -0,0 +1,12 @@
+package com.sap.cloud.s4hana.examples;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
