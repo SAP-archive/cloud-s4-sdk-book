b25380d7f4e807e5a9545b0c5a19e3b56aef4546
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-07-13 15:28:31 +0200

    Prepare testing unit

--- integration-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/BusinessPartnerServletTest.java
+++ integration-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/BusinessPartnerServletTest.java
@@ -21,51 +21,5 @@ import static org.hamcrest.Matchers.*;
 @RunWith( Arquillian.class )
 public class BusinessPartnerServletTest
 {
-    private static final MockUtil mockUtil = new MockUtil();
-    public static final String BUPA_ID = "1003764";
 
-    @ArquillianResource
-    private URL baseUrl;
-
-    @Deployment
-    public static WebArchive createDeployment()
-    {
-        return TestUtil.createDeployment(BusinessPartnerServlet.class);
-    }
-
-    @BeforeClass
-    public static void beforeClass()
-    {
-        mockUtil.mockDefaults();
-        mockUtil.mockErpDestination();
-    }
-
-    @Before
-    public void before()
-    {
-        RestAssured.baseURI = baseUrl.toExternalForm();
-    }
-
-    @Test
-    public void testGetAll()
-    {
-        when()
-                .get("/api/business-partners")
-        .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("$", hasSize(greaterThan(1)))
-                .body("[0].BusinessPartner", not(isEmptyOrNullString()));
-    }
-
-    @Test
-    public void testGetSingle() {
-        when()
-                .get("/api/business-partners?id={id}", BUPA_ID)
-        .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("BusinessPartner", allOf(not(isEmptyOrNullString()),equalTo(BUPA_ID)))
-                .body("to_BusinessPartnerAddress", hasSize(greaterThan(0)));
-    }
 }
