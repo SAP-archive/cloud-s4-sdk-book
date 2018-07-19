146e7fb39d270bd7df518eb8e5c0e6cae7ad43bf
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-07-19 15:45:54 +0200

    Update template for BusinessPartnerServletTest

--- integration-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/BusinessPartnerServletTest.java
+++ integration-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/BusinessPartnerServletTest.java
@@ -1,8 +1,8 @@
 package com.sap.cloud.s4hana.examples.addressmgr;
 
+import com.sap.cloud.sdk.testutil.MockUtil;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import com.sap.cloud.sdk.cloudplatform.connectivity.ProxyConfiguration;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -11,15 +11,17 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import java.net.URL;
 
-import com.sap.cloud.sdk.testutil.MockUtil;
-
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
 
-@RunWith( Arquillian.class )
-public class BusinessPartnerServletTest
-{
+@RunWith(Arquillian.class)
+public class BusinessPartnerServletTest {
+    public static final String BUPA_ID = "1003764";
 
 }
