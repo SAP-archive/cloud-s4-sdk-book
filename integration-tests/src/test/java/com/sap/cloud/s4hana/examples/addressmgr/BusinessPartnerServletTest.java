f19104466b226d69aa08e254d5c83cb98de7913c
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-08-27 11:38:05 +0200

    Add tests as shown in the video with failing tests for practice

--- integration-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/BusinessPartnerServletTest.java
+++ integration-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/BusinessPartnerServletTest.java
@@ -7,6 +7,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,9 +20,49 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.fail;
 
 @RunWith(Arquillian.class)
 public class BusinessPartnerServletTest {
     public static final String BUPA_ID = "1003764";
 
+    private static final MockUtil mockUtil = new MockUtil();
+
+    @ArquillianResource
+    private URL baseUrl;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return TestUtil.createDeployment(BusinessPartnerServlet.class);
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        mockUtil.mockDefaults();
+        mockUtil.mockErpDestination();
+    }
+
+    @Before
+    public void before() {
+        RestAssured.baseURI = baseUrl.toExternalForm();
+    }
+
+    @Test
+    public void testGetAll() {
+        // @formatter:off
+        when()
+                .get("/api/business-partners")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("$", hasSize(greaterThan(1)))
+                .body("[0].BusinessPartner", not(isEmptyOrNullString()));
+        // @formatter:on
+    }
+
+    @Test
+    public void testGetSingle() {
+        //TODO This test case is left as an exercise to the participant
+        fail("This test case is left as an exercise to the participant");
+    }
 }
--- unit-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommandTest.java
+++ unit-tests/src/test/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommandTest.java
@@ -7,6 +7,7 @@ import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.Busin
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerSelectable;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import com.sap.cloud.sdk.testutil.MockUtil;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,10 +19,52 @@ import java.net.URI;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class GetAllBusinessPartnersCommandTest {
+    private MockUtil mockUtil;
 
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private BusinessPartnerService service;
+
+    @Before
+    public void before() {
+        mockUtil = new MockUtil();
+        mockUtil.mockDefaults();
+        mockUtil.mockDestination("ErpQueryEndpoint", URI.create(""));
+        // Invalidate cache ahead of each test
+        new GetAllBusinessPartnersCommand(null).getCache().invalidateAll();
+    }
+
+    @Test
+    public void testGetAll() throws ODataException {
+        BusinessPartner alice = new BusinessPartner();
+        alice.setFirstName("Alice");
+        alice.setLastName("Miller");
+
+        // @formatter:off
+        when(service
+                .getAllBusinessPartner()
+                .select(any(BusinessPartnerSelectable.class))
+                .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq("1"))
+                .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
+                .execute()
+        ).thenReturn(Lists.newArrayList(alice));
+        // @formatter:on
+
+        final List<BusinessPartner> result = new GetAllBusinessPartnersCommand(service).execute();
+
+        assertEquals(1, result.size());
+        assertEquals("Alice", result.get(0).getFirstName());
+        assertEquals("MILLER", result.get(0).getLastName());
+    }
+
+    @Test
+    public void testWithError() throws ODataException {
+        //TODO This test case is left as an exercise to the participant
+        fail("This test case is left as an exercise to the participant");
+    }
 }
